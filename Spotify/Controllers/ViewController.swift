77d63cc6d31a4ed62181825789a90ccb47fdb227
Darrien Huntley <dhuntleypro@gmail.com>
2022-02-21 14:31:42 -0500

    refactored - programmatically

--- Spotify/Controllers/ViewController.swift
+++ Spotify/Controllers/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Programamic setup
+    /*
+     1. Delete Main file
+     2. In info.plist : remove - Main storyboard file base name
+     3. Application Scene Manifest .... drown down to storeboard name and remove thst
+     4. Update SceneDelegate and AppDelegate
+     
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor = .red
     }
-
-
+    
+    
 }
 
--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -10,10 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // added...
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // added...
+        let window  = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        
+        
         return true
     }
 
