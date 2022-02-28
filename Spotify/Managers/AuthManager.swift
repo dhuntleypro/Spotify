145c02b1ebb349b68906fbd680296746b8c39ec6
Darrien Huntley <dhuntleypro@gmail.com>
2022-02-28 11:06:58 -0500

    auth

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
+// OAuth 2.0 Standard
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "51d51c048fc8497197394cf214c656c8"
+        static let clientSecret = "35fbf81b1cb84a25a0fce1f4c2265e3b"
+
+    }
     private init() {}
     
     var isSignedIn: Bool {
